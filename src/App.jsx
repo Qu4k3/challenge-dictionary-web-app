9e3205205f125afef1cebc71ab8d6a4cb689b40f
Qu4k3 <paul.guillamon@gmail.com>
2023-02-03 19:09:46 +0100

    Remove template

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,13 @@
 import './App.scss';
+import { Nav } from './components/Nav';
+import { Search } from './components/Search';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Nav />
+      <Search />
+    </>
   );
 }
 
