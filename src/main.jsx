c1a7b84bf7bac08f15cc5326c0f95da00eb9c2fc
Qu4k3 <paul.guillamon@gmail.com>
2023-02-16 21:29:55 +0100

    Add responsive

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import 'normalize.css';
+import 'normalize.css'
 import './index.scss'
+import './styles/_responsive.scss'
 
 ReactDOM.createRoot(document.getElementById('dictionary')).render(
   <React.StrictMode>
