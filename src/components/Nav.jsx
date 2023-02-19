cb05357a9ef768013118b003b09cde85e30b2955
Paul Guillamon <paul.guillamon@gmail.com>
2023-02-19 17:05:05 +0100

    Fixes

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { ReactComponent as AppLogo } from "../assets/logo.svg";
 import { ReactComponent as Moon } from "../assets/icon-moon.svg";
+import { Dropdown } from './Dropdown';
 
 export function Nav() {
   const [isDarkMode, setIsDarkMode] = useState(false);
@@ -37,10 +38,6 @@ export function Nav() {
     checkLocalStorageSetState();
   }, []);
 
-  const handleFontChange = (event) => {
-    console.log('handleFontChange - event', event)
-  }
-
   return (
     <section className="nav">
       <div className="nav__logo">
@@ -48,46 +45,7 @@ export function Nav() {
       </div>
 
       <div className="nav__actions">
-        {/* <select name="select">
-          <option value="serif">Serif</option>
-          <option value="sans-serif">Sans Serif</option>
-          <option value="mono">Mono</option>
-        </select> */}
-
-        <div className="selector">
-          <div
-            className="select__control"
-            role="combobox"
-            aria-haspopup="listbox"
-            aria-labelledby="select__label"
-            aria-controls="font-theme-select__listbox"
-            aria-expanded="false"
-            tabindex="0"
-            onClick={handleFontChange}
-          >
-            <span>Sans Serif</span>
-          </div>
-
-          <div
-            role="listbox"
-            aria-labelledby="select__label"
-            tabIndex="-1"
-            className="select__popup"
-          >
-            <ul>
-              <li role="option" className="text--sans-serif" aria-selected="true">
-                Sans Serif
-              </li>
-              <li role="option" className="text--serif">
-                Serif
-              </li>
-              <li role="option" className="text--mono">
-                Mono
-              </li>
-            </ul>
-          </div>
-        </div>
-
+        <Dropdown />
         <div className="actions__theme-switch">
           <label className="switch">
             <input type="checkbox" onChange={handleSwitch} />
