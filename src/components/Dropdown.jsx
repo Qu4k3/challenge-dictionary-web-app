b391e13b827df2ef39d7823187e789fc3681786c
Qu4k3 <paul.guillamon@gmail.com>
2023-02-22 19:46:17 +0100

    Fix font label update on select

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -4,6 +4,8 @@ import { useEffect, useState } from "react";
 
 export function Dropdown() {
 
+  const [selectedFont, setSelectedFont] = useState('Sans Serif')
+
   const fonts = [
     { label: 'Serif', value: 'serif' },
     { label: 'Sans Serif', value: 'sans-serif' },
@@ -13,30 +15,36 @@ export function Dropdown() {
   const handleFontChange = (event) => {
     const fontValue = event.target.dataset.font;
     document.documentElement.setAttribute("data-font", fontValue);
+    setSelectedFont(fonts.find((e) => e.value == fontValue).label)
     localStorage.setItem("font", fontValue);
   };
 
   const checkLocalStorageSetState = () => {
     let font = localStorage.getItem("font");
 
-    switch (font) {
-      case 'serif':
-        document.documentElement.setAttribute("data-font", "serif");
-      break;
-
-      case 'sans-serif':
-        document.documentElement.setAttribute("data-font", "sans-serif");
-      break;
-
-      case 'mono':
-        document.documentElement.setAttribute("data-font", "mono");
-      break;
-    
-      default:
-        document.documentElement.setAttribute("data-font", "sans-serif");
+    if (font) {
+      setSelectedFont(fonts.find((e) => e.value == font).label)
+      
+      switch (font) {
+        case 'serif':
+          document.documentElement.setAttribute("data-font", "serif");        
         break;
+  
+        case 'sans-serif':
+          document.documentElement.setAttribute("data-font", "sans-serif");
+        break;
+  
+        case 'mono':
+          document.documentElement.setAttribute("data-font", "mono");
+        break;
+      
+        default:
+          document.documentElement.setAttribute("data-font", "sans-serif");
+          break;      
+      }
     }
 
+    
   };
 
   useEffect(() => {
@@ -45,14 +53,14 @@ export function Dropdown() {
 
   return (
     <Menu>
-      <Menu.Button className='selector__btn'><span>{document.documentElement.getAttribute("data-font")}</span><ArrowDown /></Menu.Button>
+      <Menu.Button className='selector__btn'><span>{selectedFont}</span><ArrowDown /></Menu.Button>
       <Menu.Items className='selector__items'>
         {fonts.map((font, i) => (
           <Menu.Item
             as="button"
             key={i}
             data-font={font.value}
-            className="selector__item"
+            className={`selector__item ${font.value}`}
             onClick={handleFontChange}
           >
             {font.label}
