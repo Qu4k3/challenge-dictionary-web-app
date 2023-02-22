7b6cd0c127ac22c6d38df6ff32f106e246fabca5
Qu4k3 <paul.guillamon@gmail.com>
2023-02-22 20:11:06 +0100

    Fix manifest

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,26 @@ export default defineConfig({
   plugins: [
     react(),
     svgr(),
-    VitePWA({ registerType: 'autoUpdate' })
+    VitePWA({
+      includeAssets: ['favicon-32x32.png', 'logo.png'],
+      manifest: {
+        name: 'Dictionary Web App',
+        short_name: 'Dictionary',
+        description: 'Dictionary Web App Challenge',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'logo.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'logo.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
   ]
 })
